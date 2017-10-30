585b73229d051b03799c5145f955cfae74426c59
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 19:32:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -16,7 +16,7 @@ class Backer
   def back_project(proj)
     #proj = Project.find_or_create_by_name(name)
     self.backed_projects << proj
-    proj.backed_by(self)
+    proj.add_backer(self)
   end
 
   def self.all
