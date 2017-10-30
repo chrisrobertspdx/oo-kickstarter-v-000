3e798e48a0bb39a641fac31baf2b99528bc2768b
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 19:08:46 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,20 @@
 class Backer
   attr_accessor :name, :backed_projects
 
+  @@backers = []
+
   def initialize(name)
     @name = name
     @backed_projects = []
+    self.save
   end
+
+  def save
+    @@backers << self
+  end
+  
+  def back_project(name)
+
+  end
+  
 end
