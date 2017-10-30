1d79c4de5b13017b6a375bacd121ec0bbd21e2ab
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 19:21:04 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,24 @@
 class Project
   attr_accessor :name, :backers
+  @@projects = []
   def initialize(name)
     @name = name
     @backers = []
+  end
+  def save
+    @@projects << self
+  end
+  def backed_by(backer)
+    @backers << backer
+  end
+  def self.find_or_create_by_name(name)
+    project_list = @@projects.select{|e| e.name == name}
+    if project_list.size > 0
+       project_list[0]
+    else
+      instance = Project.new(name)
+      instance.save
+      instance
+    end
   end
 end
